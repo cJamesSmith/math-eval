8e420bae1fdce98efa0a61be0d18daecfca9db37
Chenxw <xianwei.chen@connect.polyu.hk>
2025-05-13 19:44:57 +0800

    split

---
+++ LIMO-eval/eval.sh
@@ -0,0 +1,19 @@
+PT=$1
+TOKENIZERS_PARALLELISM=false \
+CUDA_VISIBLE_DEVICES=$2 \
+VLLM_WORKER_MULTIPROC_METHOD=spawn \
+python eval.py \
+--model_name_or_path "Qwen/Qwen2.5-7B" \
+--data_name "amc" \
+--prompt_type "${PT}" \
+--temperature 0.6 \
+--start_idx 0 \
+--end_idx -1 \
+--n_sampling 1024 \
+--k 256 \
+--split "test" \
+--max_tokens 16384 \
+--seed 0 \
+--top_p 0.95 \
+--surround_with_messages \
+
---
+++ LIMO-eval/prompts/long-instruct/gpqa.py
@@ -0,0 +1,7 @@
+
+
+system_prompt = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The assistant should 1) Identify core concepts and required formulas. 2) Break down solutions into logical, numbered steps. 3) Verify results using alternative methods or substitutions. Put your final answer within \\boxed{}."
+
+few_shot_prompt = ""
+
+question_format = """{question}"""
